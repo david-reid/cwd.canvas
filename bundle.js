9a09ac800a46a8e601fe7d08be08008027c7a04f
David Reid <david@admiredigital.com>
2020-09-28 16:39:02 +0100

    Triangles, lines and gradients

--- bundle.js
+++ bundle.js
@@ -4,8 +4,15 @@ window.onload = function() {
     let graphics = new Graphics("canvas");
     graphics.makeFullScreen();
 
-    graphics.fillCanvas("#1bafdb");
+    let gradient = graphics.createTwoColorGradient(
+        "#4c5c96","#1bafdb",0,0, 0,graphics.dimensions().height * 0.66);
+
+    graphics.fillCanvas(gradient);
     graphics.strokeRectangle(50,50, 150,100, "#ffffff", 5);
     graphics.fillCircle(300,100, 75, "#ffffff");
     graphics.strokeCircle(475,100, 75, "#ffffff", 5);
+
+    graphics.fillTriangle(125,300, 150, "#ffffff");
+    graphics.strokeTriangle(300,300, 150, "#ffffff", 5);
+    graphics.stroke(0,500, graphics.dimensions().width,500, "#ffffff", 15, [20,10]);
 }
--- graphics.js
+++ graphics.js
@@ -27,6 +27,16 @@ class Graphics {
         this.context.fillRect(x,y, width, height);
     }
 
+    fillTriangle(x,y, size, color) {
+        this.context.fillStyle = color;
+        this.context.beginPath();
+        this.context.moveTo(x, y-size/2);
+        this.context.lineTo(x+size/2, y+size/2);
+        this.context.lineTo(x-size/2, y+size/2);
+        this.context.lineTo(x, y-size/2);
+        this.context.fill();
+    }
+
     fillCircle(x,y, size, color) {
         this.context.fillStyle = color;
         this.context.beginPath();
@@ -46,5 +56,35 @@ class Graphics {
         this.context.beginPath();
         this.context.arc(x,y, size, 0, Math.PI*2);
         this.context.stroke();
+    }
+
+    strokeTriangle(x,y, size, color, lineWidth) {
+        this.context.strokeStyle = color;
+        this.context.lineWidth = lineWidth;
+        this.context.beginPath();
+        this.context.moveTo(x, y-size/2);
+        this.context.lineTo(x+size/2, y+size/2);
+        this.context.lineTo(x-size/2, y+size/2);
+        this.context.lineTo(x, y-size/2);
+        this.context.stroke();
+    }
+
+    stroke(p1x,p1y, p2x,p2y, color, lineWidth, pattern) {
+        this.context.strokeStyle = color;
+        this.context.lineWidth = lineWidth;
+        if ( pattern ) {
+            this.context.setLineDash(pattern);
+        }
+        this.context.beginPath();
+        this.context.moveTo(p1x, p1y);
+        this.context.lineTo(p2x,p2y);
+        this.context.stroke();
+    }
+
+    createTwoColorGradient(color1,color2, p1x,p1y, p2x,p2y) {
+        let gradient = this.context.createLinearGradient(p1x,p1y, p2x,p2y);
+        gradient.addColorStop(0,color1);
+        gradient.addColorStop(1,color2);
+        return gradient;
     }
 }
