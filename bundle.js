39ee7ca758cb34c154ba72b4a3bfae6c4359d037
David Reid <david@admiredigital.com>
2020-10-18 08:04:16 +0100

    Adds a simple game loop

--- bundle.js
+++ bundle.js
@@ -1,16 +1,28 @@
+let graphics;
+let clouds;
+let mountains;
+
 window.onload = function() {
     console.log("HTML5 Canvas API for Games");
-
-    let graphics = new Graphics("canvas");
+    graphics = new Graphics("canvas");
     graphics.makeFullScreen();
+    clouds = new Clouds(graphics);
+    mountains = new Mountains(graphics);
+    this.setInterval(update, 1000/60);
+}
 
+function update() {
+    moveEverything();
+    drawEverything();
+}
+
+function drawEverything() {
     let gradient = graphics.createTwoColorGradient(
         "#4c5c96","#1bafdb",0,0, 0,graphics.dimensions().height * 0.66);
     graphics.fillCanvas(gradient);
-
-    let clouds = new Clouds(graphics);
-    let mountains = new Mountains(graphics);
-
     clouds.draw();
     mountains.draw();
+}
+
+function moveEverything() {
 }
